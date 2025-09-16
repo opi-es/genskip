29a52a286d6e1efe813085a5fda8e9eb297cb623
Arthur Morozov <artursvoboda193@gmail.com>
2025-09-16 17:37:08 +0400

    изменил интервал между нажатием и скорректировал проверку

--- main.go
+++ main.go
@@ -14,12 +14,12 @@ var (
 
 // Константы для кодов клавиш
 const (
-	VK_F10 = 0x79
-	VK_F   = 0x46
+	VK_F9 = 0x78
+	VK_F  = 0x46
 )
 
 func pressF() {
-	// Key down (пробел = 0x20)
+	// Key down (F = 0x46)
 	keybdEvent.Call(uintptr(VK_F), 0, 0, 0)
 	time.Sleep(10 * time.Millisecond)
 	// Key up
@@ -29,41 +29,44 @@ func pressF() {
 // isKeyPressed проверяет, нажата ли указанная клавиша
 func isKeyPressed(keyCode int) bool {
 	state, _, _ := getAsyncKeyState.Call(uintptr(keyCode))
-	return state&0x8000 != 0
+	return uint16(state)&0x8000 != 0
 }
 
 func main() {
-	fmt.Println("Программа запущена. Нажмите F10 для паузы/продолжения.")
+	fmt.Println("Программа запущена. Нажмите F9 для паузы/продолжения.")
 
 	paused := false
-	keyPressed := false // Для отслеживания состояния клавиши
+	keyPressed := false // для отслеживания состояния клавиши
 
-	// Даем время переключиться на окно игры
-	time.Sleep(3 * time.Second)
+	// даем время переключиться на окно игры
+	time.Sleep(5 * time.Second)
+	// таймер для периода между нажатиями Ф
+	tickerAction := time.NewTicker(10 * time.Second)
+	defer tickerAction.Stop()
+	// тикер для опроса клавиш
+	tickerPoll := time.NewTicker(50 * time.Millisecond)
+	defer tickerPoll.Stop()
 
 	for {
-		// Проверяем, нажата ли F10
-		if isKeyPressed(VK_F10) {
-			if !keyPressed {
-				// Меняем состояние только если клавиша только что нажата
-				paused = !paused
-				if paused {
-					fmt.Println("Программа приостановлена. Нажмите F10 для продолжения.")
-				} else {
-					fmt.Println("Программа продолжена.")
+		select {
+		case <-tickerPoll.C:
+			if isKeyPressed(VK_F9) {
+				if !keyPressed {
+					paused = !paused
+					if paused {
+						fmt.Println("Пауза. Нажмите F9 для продолжения.")
+					} else {
+						fmt.Println("Продолжение. Нажмите F9 для паузы.")
+					}
+					keyPressed = true
 				}
-				keyPressed = true
+			} else {
+				keyPressed = false
 			}
-		} else {
-			keyPressed = false
+		case <-tickerAction.C:
+			if !paused {
+				pressF()
+			}
 		}
-
-		// Если не на паузе, нажимаем пробел
-		if !paused {
-			pressF()
-		}
-
-		// Ждем немного перед следующей итерацией
-		time.Sleep(100 * time.Millisecond)
 	}
 }
